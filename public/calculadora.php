16da5b7d9e741248346208e885a915a120b0f0bd
aleelus <aleelus@hotmail.com>
2018-03-14 19:16:13 -0300

    calculadora iotas - masomeno

--- public/calculadora.php
+++ public/calculadora.php
@@ -30,14 +30,47 @@ if(!isset($_SESSION['user']) || $_SESSION['user']=="invitado")
 						<section class="wrapper style1 special">
               <h3>Calculadora</h3>
 							<div class="inner">
-                <div class="row">
-                  <div class="col-md-4">
-                    <input type="text" name="user" id="user" pattern="[0-9]{1,15}"  required class="form-control input-lg" placeholder="Cantidad a invertir (AR$)"/>
+                <form name="form" action="" method="post">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <input type="text" name="cantInvertir" id="cantInvertir" pattern="[0-9]{1,15}"  required class="form-control input-lg" placeholder="Cantidad a invertir (AR$)"/>
+                    </div>
+                    <div class="col-md-8">
+                      <?php
+                      //extrae el precio de iota y dolar
+                      $comision_ripio_rapipago = 1 - 0.025;
+                      $comision_ripio_transferencia_bancaria= 0.99;
+                      $comision_ripio_compra_bitcoin = 1-0.005;
+                      $comision_transaccion_bitcoin = 60;//pesos argentinos
+
+                      $ch = curl_init();
+                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                      curl_setopt($ch, CURLOPT_URL, 'https://widgets.coinmarketcap.com/v1/ticker/iota/?convert=USD');
+                      $result = curl_exec($ch);
+                      curl_close($ch);
+                      $obj = json_decode($result);
+
+                      $data_in = "http://ws.geeklab.com.ar/dolar/get-dolar-json.php";
+                      $data_json = @file_get_contents($data_in);
+                      if(strlen($data_json)>0)
+                      {
+                        $data_out = json_decode($data_json,true);
+
+                        if(isset($_POST["cantInvertir"])){
+                          $total_pesos = ($_POST["cantInvertir"]*$comision_ripio_rapipago*$comision_ripio_compra_bitcoin)-$comision_transaccion_bitcoin;
+                          echo 'Obtengo '.round($obj[0]->price_usd*$total_pesos/$data_out['libre'],2).' Miotas';
+                        }
+                      }
+
+
+
+
+                      ///////////////////////////////////////////////////////////
+                       ?>
+                    </div>
                   </div>
-                  <div class="col-md-8">
-                    <p>(☞ﾟヮﾟ)☞ Aca va la cantidad de miotas equivalentes ☜(ﾟヮﾟ☜)</p>
-                  </div>
-                </div>
+                </form>
 							</div>
 						</section>
 					</article>
